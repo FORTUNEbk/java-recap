88bc517fd5c020c0771234b10be0d1ed68baa285
Kanyangi Fortune <kanyangif@gmail.com>
2025-09-10 14:46:22 +0300

    Collect and count the number of even and odd numbers

--- EvenAndOdd.java
+++ EvenAndOdd.java
@@ -9,6 +9,8 @@ public void Numbers(){
     Scanner scanner = new Scanner(System.in);
 
     ArrayList<Integer> nums = new ArrayList<>();
+    ArrayList<Integer> evenList = new ArrayList<>();
+    ArrayList<Integer> oddList = new ArrayList<>();
 
     while(true){
     System.out.println("Enter your values here(or enter -1 to exit!): ");
@@ -18,17 +20,23 @@ public void Numbers(){
         System.out.println("Bye!");
         break;
     }
+    
     nums.add(input);
         
     }
     for(int i = 0; i < nums.size(); i++){
         if(nums.get(i) % 2 == 0){
+            evenList.add(nums.get(i));
             even++;
         }else{
+        oddList.add(nums.get(i));
         odd++;
         }
+       
     }
 
+    System.out.println("The even numbers are: " + evenList);
+    System.out.println("The odd numbers are: " + oddList);
     System.out.println("Even numbers are: " + even + " Odd numbers are: " + odd);
    
  }
